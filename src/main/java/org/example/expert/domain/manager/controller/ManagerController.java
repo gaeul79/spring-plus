0a2ddf9fc8f827260b56f0921eab26f46279e82e
guguggagga <gaeul79_@naver.com>
2024-11-13 15:08:29 +0900

    feat: [Level3-4] 대용량 데이터 처리 - 코드 정리

--- src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
+++ src/main/java/org/example/expert/domain/manager/controller/ManagerController.java
@@ -2,20 +2,15 @@ package org.example.expert.domain.manager.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.manager.dto.request.ManagerSaveRequest;
 import org.example.expert.domain.manager.dto.response.ManagerResponse;
 import org.example.expert.domain.manager.service.ManagerService;
 import org.example.expert.security.UserDetailsImpl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,7 +23,7 @@ public class ManagerController {
             @PathVariable long todoId,
             @Valid @RequestBody ManagerSaveRequest managerSaveRequest
     ) {
-         return ResponseEntity.ok(managerService.saveManager(userDetail.getId(), todoId, managerSaveRequest));
+        return ResponseEntity.ok(managerService.saveManager(userDetail.getId(), todoId, managerSaveRequest));
     }
 
     @GetMapping("/todos/{todoId}/managers")
