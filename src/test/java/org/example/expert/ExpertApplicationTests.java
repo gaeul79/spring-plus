49fd9151afc96fa2db279780ee6fcb90956c4dd9
guguggagga <gaeul79_@naver.com>
2024-11-19 16:34:04 +0900

    feat: [Level3-3] AWS 활용 마스터 - 배포 테스트를 위해 dummy데이터를 밀어넣는 함수 주석처리

--- src/test/java/org/example/expert/ExpertApplicationTests.java
+++ src/test/java/org/example/expert/ExpertApplicationTests.java
@@ -1,17 +1,11 @@
 package org.example.expert;
 
-import org.apache.commons.lang3.RandomStringUtils;
-import org.example.expert.domain.user.entity.User;
-import org.example.expert.domain.user.enums.UserRole;
 import org.example.expert.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.Rollback;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class ExpertApplicationTests {
@@ -28,33 +22,33 @@ class ExpertApplicationTests {
     @Test
     @Rollback(false)
     void sampleDataUser() {
-        for (int idx = 812778; idx < 1000000; idx++) {
-            String random = RandomStringUtils.randomAlphanumeric(10);
-            String nickname = "[user " + idx + "]" + random;
-            userRepository.save(User.builder()
-                    .email(nickname + "@gmail.com")
-                    .password(passwordEncoder.encode("1234"))
-                    .nickname(nickname)
-                    .userRole(UserRole.USER)
-                    .build());
-        }
+//        for (int idx = 812778; idx < 1000000; idx++) {
+//            String random = RandomStringUtils.randomAlphanumeric(10);
+//            String nickname = "[user " + idx + "]" + random;
+//            userRepository.save(User.builder()
+//                    .email(nickname + "@gmail.com")
+//                    .password(passwordEncoder.encode("1234"))
+//                    .nickname(nickname)
+//                    .userRole(UserRole.USER)
+//                    .build());
+//        }
     }
 
     @Test
     void findUser() {
-        String nickname = "[user 999999]a9EEHZ7KDg";
-        User user = userRepository.findByNickname(nickname);
+//        String nickname = "[user 999999]a9EEHZ7KDg";
+//        User user = userRepository.findByNickname(nickname);
     }
 
     @Test
     void findUser2() {
-        String nickname = "[user 999999]a9EEHZ7KDg";
-        User user = userRepository.findByMinimumColumnNickName(nickname);
+//        String nickname = "[user 999999]a9EEHZ7KDg";
+//        User user = userRepository.findByMinimumColumnNickName(nickname);
     }
 
     @Test
     void findUser3() {
-        String nickname = "[user 999999]a9EEHZ7KDg";
-        User user = userRepository.findByIndexingNickName(nickname);
+//        String nickname = "[user 999999]a9EEHZ7KDg";
+//        User user = userRepository.findByIndexingNickName(nickname);
     }
 }
