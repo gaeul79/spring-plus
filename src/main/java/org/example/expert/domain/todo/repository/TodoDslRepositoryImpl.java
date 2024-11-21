950f57935ac9e4c0866c2370bd0a4da9610c8897
guguggagga <gaeul79_@naver.com>
2024-11-21 20:16:36 +0900

    refactor: 과해 해설 반영 - BooleanExpression을 활용하여 검색조건 만드는거 일부 적용

--- src/main/java/org/example/expert/domain/todo/repository/TodoDslRepositoryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoDslRepositoryImpl.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.todo.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -14,17 +15,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 public class TodoDslRepositoryImpl implements TodoDslRepository {
     private final JPAQueryFactory queryFactory;
+    private final QTodo todo = new QTodo("todo");
+    private final QUser user = new QUser("user"); // 별칭
 
     @Override
     public Optional<Todo> findByIdWithUser(Long todoId) {
-        QTodo todo = new QTodo("todo");
-        QUser user = new QUser("user"); // 별칭
+
 
         // fetchFirst() : 첫 번째 결과를 가져오거나 결과가 없으면 null을 가져옵니다.
         // fetchOne() : 결과가 여러건이면 fail이 난다.
@@ -43,7 +46,11 @@ public class TodoDslRepositoryImpl implements TodoDslRepository {
 
         JPAQuery<Long> cntTotal = queryFactory.select(todo.count()).from(todo)
                 .leftJoin(todo.user, user)
-                .where(makeWhere(requestDto));
+                .where(likeTitle(requestDto.getTitle()),
+                       likeNickName(requestDto.getNickname()),
+                       likeWeather(requestDto.getWeather()),
+                       goeCreateDate(requestDto.getStartCreateDate()),
+                       loeCreateDate(requestDto.getEndCreateDate()));
         List<TodoDetailResponse> todos = queryFactory.selectFrom(todo)
                 .leftJoin(todo.user, user)
                 .where(makeWhere(requestDto))
@@ -59,8 +66,6 @@ public class TodoDslRepositoryImpl implements TodoDslRepository {
 
     private BooleanBuilder makeWhere(TodoSearchRequest requestDto) {
         BooleanBuilder builder = new BooleanBuilder();
-        QUser user = QUser.user;
-        QTodo todo = QTodo.todo;
 
         if (StringUtils.hasText(requestDto.getTitle()))
             builder.and(todo.title.contains(requestDto.getTitle()));
@@ -77,5 +82,35 @@ public class TodoDslRepositoryImpl implements TodoDslRepository {
         }
 
         return builder;
+    }
+
+    private BooleanExpression likeTitle(String title) {
+        if (title == null || title.isEmpty())
+            return null;
+        return todo.title.containsIgnoreCase(title);
+    }
+
+    private BooleanExpression likeNickName(String nickname) {
+        if (nickname == null || nickname.isEmpty())
+            return null;
+        return user.nickname.containsIgnoreCase(nickname);
+    }
+
+    private BooleanExpression likeWeather(String weather) {
+        if (weather == null || weather.isEmpty())
+            return null;
+        return todo.weather.containsIgnoreCase(weather);
+    }
+
+    private BooleanExpression goeCreateDate(LocalDateTime startDate) {
+        if (startDate == null)
+            return null;
+        return todo.createdAt.goe(startDate);
+    }
+
+    private BooleanExpression loeCreateDate(LocalDateTime endDate) {
+        if (endDate == null)
+            return null;
+        return todo.createdAt.loe(endDate);
     }
 }
